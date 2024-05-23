0e99ec4b7065bbc13c1c25b4ea39442eb3d59869
Kasuni Wijeratne <129731388+KasuniWijeratne@users.noreply.github.com>
2024-05-23 21:54:34 +0530

    Game Over and Retry

---
+++ Energy_Elixer/Assets/Codes/GamePlay/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool isGameOver;
+    public GameObject gameOverPanel;
+    private void Awake(){
+        isGameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGameOver){
+            gameOverPanel.SetActive(true);
+        }
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Energy_Elixer/Assets/Codes/GamePlay/GameOver.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Water")
+        {
+            GameManager.isGameOver = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
