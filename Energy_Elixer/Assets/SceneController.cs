0f3e5c88d38349b25fa752caa27eab48ec0ed8c2
HimanshiDeSilva <himanshidesilva2000@gmail.com>
2024-06-04 18:52:24 +0530

    Adding Scene Transition between level 1 and level 2

---
+++ Energy_Elixer/Assets/SceneController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneController : MonoBehaviour
+{
+
+    public static SceneController instance;
+    [SerializeField] Animator transitionAnim;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    
+    }
+    public void NextLevel()
+    {
+        StartCoroutine(LoadLevel());
+       
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        transitionAnim.SetTrigger("start");
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+
+
+    }
+}
